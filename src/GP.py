b22af93087d28f4e5eddc3ab107814a6d08518fb
franciscovargas <vargfran@gmail.com>
2021-01-14 00:47:06 +0000

    Added Nystrom approximation

--- src/GP.py
+++ src/GP.py
@@ -1,5 +1,6 @@
 import torch
 import pyro.contrib.gp as gp
+import pyro
 import math
 
 class MultitaskGPModel():
@@ -23,12 +24,76 @@ class MultitaskGPModel():
     
     def fit_gp(self,gpr, num_steps=100):
         raise("To be implemented")
-        #optimizer = torch.optim.Adam(gpr.parameters(), lr=0.005)
-        #loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
-        #losses = []
-        #for i in range(num_steps):
-        #    optimizer.zero_grad()
-        #    loss = loss_fn(gpr.model, gpr.guide)
-        #    loss.backward()
-        #    optimizer.step()
-        #    losses.append(loss.item())
+
+        
+class MultitaskGPModelSparse(MultitaskGPModel):
+    @staticmethod
+    def create_inducing_points(X, num_data_points, num_time_points):
+        time_slice = X[:,-1]
+        t0, t1 = time_slice.min().item(), time_slice.max().item()
+
+        X0, X1 = X[:,:-1].min(0).values, X[:,:-1].max(0).values
+
+        X01 = X1 - X0
+        lin = torch.linspace(0,1, num_time_points)
+        space_points = X0 + lin.reshape(-1,1) * X01
+
+        space_points_n = space_points.repeat(num_data_points,1)
+        time_points = torch.linspace(t0,t1, num_time_points).repeat(num_data_points).reshape(-1,1)
+
+        return torch.cat((space_points_n, time_points), 1)
+    
+    
+    @staticmethod
+    def create_inducing_points_nystrom(X, num_data_points, num_time_points):
+        # First we sample the time series and then we sample time indices for each series         
+
+        # Assuming series came in order we first find the original number of timepoints
+        _max = X[:,-1].max().item()
+        original_time_length = torch.where(X[:,-1] == _max)[0][0].item()  + 1
+
+        # Sample a timeseries
+        perm = torch.randperm(int(X.size(0) / original_time_length))
+        idx = perm[:num_data_points]
+        idxs = ((idx * original_time_length).reshape(-1,1) + torch.arange(original_time_length)).flatten()
+        samples = X[idxs, :]
+
+        # In expectation approach:             
+        # out_samps  = out_samps[(samples.uniform_() > 1.0 - num_time_points * 1.0 /_max)]
+
+        # Sample timepoints for each timeseries
+        prob_dist = torch.ones((num_data_points, original_time_length))
+        inx_matrix = torch.multinomial(prob_dist, num_time_points, replacement=False) * torch.arange(1, num_data_points+1).reshape(-1,1)
+        out_samps = samples[inx_matrix.flatten(),:]
+     
+        return out_samps
+        
+        
+        
+    def __init__(self, X, y, noise=.1, dt=1, num_data_points=10, num_time_points=50, nystrom_only=True):
+        self.dim = y.shape[1]
+        self.gpr_list = []
+        
+        self.nystrom_only = nystrom_only
+        Xu = self.create_inducing_points_nystrom(X, num_data_points, num_time_points)
+        
+        for i in range(y.shape[1]):
+            #kernel = gp.kernels.RBF(input_dim=X.shape[1], variance=torch.tensor(1),
+            #                        lengthscale=torch.tensor(1.))
+            kernel = gp.kernels.Matern32(input_dim=X.shape[1])
+            gpr = gp.models.SparseGPRegression(X, y[:, i], kernel, noise=torch.tensor(noise / math.sqrt(dt)), Xu=Xu)
+            self.gpr_list.append(gpr)
+    
+    def fit_gp(self, num_steps=30):
+        if self.nystrom_only: return
+        
+        for gpr in self.gpr_list:
+            optimizer = torch.optim.Adam(gpr.parameters(), lr=0.005)
+            loss_fn = pyro.infer.Trace_ELBO().differentiable_loss
+            losses = []
+            for i in range(num_steps):
+                optimizer.zero_grad()
+                loss = loss_fn(gpr.model, gpr.guide)
+                loss.backward(retain_graph=True)
+                optimizer.step()
+                losses.append(loss.item())
--- src/MLE_drift.py
+++ src/MLE_drift.py
@@ -1,15 +1,19 @@
 
 import torch
 from SDE_solver import solve_sde_RK
-from GP import MultitaskGPModel
+from GP import MultitaskGPModel, MultitaskGPModelSparse
 from tqdm import tqdm
 import gc
 
-def fit_drift(Xts,N,dt):
+def fit_drift(Xts,N,dt, sparse=False, num_data_points=10, num_time_points=50):
     X_0 = Xts[:, 0, 0].reshape(-1, 1)  # Extract starting point
     Ys = ((Xts[:, 1:, :-1] - Xts[:, :-1, :-1]) / dt).reshape((-1, Xts.shape[2] - 1)) # Autoregressive targets y = (X_{t+e} - X_t)/dt
-    Xs = Xts[:, :-1, :].reshape((-1, Xts.shape[2])) #Drop the last timepoint in each timeseries 
-    gp_drift_model = MultitaskGPModel(Xs, Ys)  # Setup the GP
+    Xs = Xts[:, :-1, :].reshape((-1, Xts.shape[2])) # Drop the last timepoint in each timeseries
+    if sparse:
+        gp_drift_model = MultitaskGPModelSparse(Xs, Ys, num_data_points=num_data_points, num_time_points=num_time_points)
+        gp_drift_model.fit_gp()
+    else:
+        gp_drift_model = MultitaskGPModel(Xs, Ys)  # Setup the GP
     # fit_gp(gp_drift_model, num_steps=5) # Fit the drift
     gp_ou_drift = lambda x: gp_drift_model.predict(x)  # Extract mean drift
     return gp_ou_drift
@@ -17,19 +21,26 @@ def fit_drift(Xts,N,dt):
 
 
 
-def MLE_IPFP(X_0,X_1,N=10,sigma=1,iteration=10,prior_drift=None):
-    if prior_drift == None:
+def MLE_IPFP(
+        X_0,X_1,N=10,sigma=1,iteration=10, prior_drift=None,
+        sparse=False, num_data_points=10, num_time_points=50, prior_X_0=None
+    ):
+    if prior_drift is None:
         prior_drift = lambda x: torch.tensor([0]*X_0.shape[1]).reshape((1,-1)).repeat(X_0.shape[0],1)
+        
+    if prior_X_0 is None:
+        prior_X_0 = X_0
+        
     dt = 1.0 / N
-
+    
     # Estimating the backward drift of brownian motion
-    # Start in X_0 and go forward. Then flip the series and learn a backward drift: drift_backward
+    # Start in prior_X_0 and go forward. Then flip the series and learn a backward drift: drift_backward
 
-    t, Xts = solve_sde_RK(b_drift=prior_drift, sigma=sigma, X0=X_0, dt=dt, N=N)
+    t, Xts = solve_sde_RK(b_drift=prior_drift, sigma=sigma, X0=prior_X_0, dt=dt, N=N)
     #plot_trajectories_2(Xts, t)
 
     Xts[:,:,:-1] = Xts[:,:,:-1].flip(1) # Reverse the series
-    drift_backward = fit_drift(Xts,N=N,dt=dt)
+    drift_backward = fit_drift(Xts,N=N,dt=dt,sparse=sparse,num_data_points=num_data_points, num_time_points=num_time_points)
 
     result = []
     for i in tqdm(range(iteration)):
@@ -40,8 +51,8 @@ def MLE_IPFP(X_0,X_1,N=10,sigma=1,iteration=10,prior_drift=None):
 
         # Reverse the series
         Xts[:,:,:-1] = Xts[:,:,:-1].flip(1)
-        drift_forward = fit_drift(Xts,N=N,dt=dt)
-        gc.collect()
+        drift_forward = fit_drift(Xts,N=N,dt=dt,sparse=sparse, num_data_points=num_data_points, num_time_points=num_time_points)
+        gc.collect() # fixes odd memory leak
 
         # Estimate backward drift
         # Start from X_0 and roll until t=1 using drift_forward
@@ -49,11 +60,11 @@ def MLE_IPFP(X_0,X_1,N=10,sigma=1,iteration=10,prior_drift=None):
 
         # Reverse the series
         Xts[:,:,:-1] = Xts[:,:,:-1].flip(1)
-        drift_backward = fit_drift(Xts,N=N,dt=dt)
+        drift_backward = fit_drift(Xts,N=N,dt=dt,sparse=sparse, num_data_points=num_data_points, num_time_points=num_time_points)
 
 
         T, M = solve_sde_RK(b_drift=drift_forward, sigma=sigma, X0=X_0,dt=dt, N=N)
         T2, M2 = solve_sde_RK(b_drift=drift_backward, sigma=sigma, X0=X_1,dt=dt, N=N)
         result.append([T,M,T2,M2])
-        gc.collect()
+        gc.collect() # fixes odd memory leak
     return result
