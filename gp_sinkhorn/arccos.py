77fd0d04da47f025a273147263ad49bee4e0e093
archertaps <44512926+archertaps@users.noreply.github.com>
2022-04-13 01:28:00 +0100

    Working implementation of arccos kernel

--- gp_sinkhorn/arccos.py
+++ gp_sinkhorn/arccos.py
@@ -1,16 +1,22 @@
-from pyro.nn.module import PyroParam
-from torch.distributions import constraints
 
 import torch
-import numpy as np
-import math
+
+from pyro.nn.module import PyroParam
+from torch.distributions import constraints
 from pyro.contrib.gp.kernels import Kernel
 
 
 class ArcCos(Kernel):
     
-    def __init__(self, input_dim, variance=None, lengthscale=None, active_dims=None):
+    def __init__(self, input_dim, variance_w, variance_b, variance=None, 
+                 lengthscale=None, active_dims=None):
         super().__init__(input_dim, active_dims)
+
+        self.variance_w = variance_w
+        self.variance_w = PyroParam(variance_w, constraints.positive)
+        
+        self.variance_b = variance_b
+        self.variance_b = PyroParam(variance_b, constraints.positive)
 
         variance = torch.tensor(1.0) if variance is None else variance
         self.variance = PyroParam(variance, constraints.positive)
@@ -18,55 +24,25 @@ class ArcCos(Kernel):
         lengthscale = torch.tensor(1.0) if lengthscale is None else lengthscale
         self.lengthscale = PyroParam(lengthscale, constraints.positive)
     
+    def transform(self, x):
+        return self.variance_w * x + self.variance_b
+
     def forward(self, X, Z=None):
+        """ Compute kernel matrix. """
         if Z is None:
             Z = X
-        t = X.mm(Z.t())
-        X_norm = torch.linalg.norm(X, dim=1)
-        Z_norm = torch.linalg.norm(Z, dim=1)
+        xz = self.transform(X.mm(Z.T))
+        
+        X_norm = torch.sqrt(self.transform(torch.linalg.norm(X, dim=1) ** 2))
+        Z_norm = torch.sqrt(self.transform(torch.linalg.norm(Z, dim=1) ** 2))
 
-        # Can be more memory efficient; but that's not a problem here
-        multiplier = X_norm.outer(Z_norm)
+        multiplier = torch.outer(X_norm, Z_norm)
 
-        thetas = torch.acos(torch.clip(t / multiplier, -1, 1))
-            
-        # Have to manually tune these for now, easier API-wise than a partial
-        # function setting the init args
-        sigma_b = 1
-        sigma_w = 1
+        cos_theta = torch.clip(xz / multiplier, -1, 1)
+        sin_theta = torch.sqrt(1 - cos_theta ** 2)
 
-        J = torch.sin(thetas) + (torch.pi - thetas) * torch.cos(thetas)
-        return self.variance * (sigma_w * J * multiplier / torch.pi) + sigma_b
+        J = sin_theta + (torch.pi - torch.acos(cos_theta)) * cos_theta
 
-
-    def forward2(self, X, Z=None):
-        same = False
-        if Z is None:
-            same = True
-            Z = X
-
-        xy = X.mm(Z.t())
-        xx = X.mm(X.t())
-        yy = Z.mm(Z.t())
-
-        f32_tiny = np.finfo(np.float32).tiny
-
-        xx_yy = xx * yy + f32_tiny
-
-        # Clamp these so the outputs are not NaN
-        cos_theta = (xy * xx_yy.rsqrt()).clamp(-1, 1)
-        sin_theta = torch.sqrt((xx_yy - xy**2).clamp(min=0))
-        theta = torch.acos(cos_theta)
-        xy = (sin_theta + (math.pi - theta)*xy) / (2*math.pi)
-
-        xx = xx/2.
-        if same:
-            yy = xx
-            
-            # Make sure the diagonal agrees with `xx`
-            eye = torch.eye(xy.size()[0]).unsqueeze(-1).unsqueeze(-1)
-            xy = (1-eye)*xy + eye*xx
-        else:
-            yy = yy/2.
-        return xy
-
+        # TODO: does it matter whether the denominator is pi or 2 * pi?
+        # CNN-GP folk use 2 * pi, but pi is also believable and works.
+        return self.variance * J * multiplier / (2 * torch.pi)
